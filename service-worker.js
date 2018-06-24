136559888ee34e7f45cfa5413dafe05ccdfb28d5
Evan Cohen <evanbtcohen@gmail.com>
2018-06-24 23:58:58 +0200

    Beograd

--- service-worker.js
+++ service-worker.js
@@ -12,36 +12,36 @@ const CACHE_STATIC = [
     '/assets/images/bullet.png',
     '/assets/images/nav-bg.gif',
     '/assets/images/hr.gif'
- ];
+];
 
- const CACHE_APP = [
+const CACHE_APP = [
     '/',
     '/index.html',
- ];
+];
 
-self.addEventListener('install',function(e){
+self.addEventListener('install', function (e) {
     e.waitUntil(
         Promise.all([
             caches.open(STATIC_CACHE_NAME),
             caches.open(APP_CACHE_NAME),
             self.skipWaiting()
-          ]).then(function(storage){
+        ]).then(function (storage) {
             var static_cache = storage[0];
             var app_cache = storage[1];
             return Promise.all([
-              static_cache.addAll(CACHE_STATIC),
-              app_cache.addAll(CACHE_APP)]);
+                static_cache.addAll(CACHE_STATIC),
+                app_cache.addAll(CACHE_APP)]);
         })
     );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(
         Promise.all([
             self.clients.claim(),
-            caches.keys().then(function(keyList) {
+            caches.keys().then(function (keyList) {
                 return Promise.all(
-                    keyList.map(function(key) {
+                    keyList.map(function (key) {
                         if (key !== APP_CACHE_NAME && key !== STATIC_CACHE_NAME) {
                             console.log('deleting', key);
                             return caches.delete(key);
@@ -53,18 +53,26 @@ self.addEventListener('activate', function(e) {
     );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function (event) {
     console.log('Fetch event fired.', event.request.url);
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) {
-				console.log('Retrieving from cache...');
-				return response;
-			}
-			console.log('Retrieving from URL...');
-			return fetch(event.request).catch(function(event){
-				console.log('Fetch request failed!');
-			});
-		})
-	);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // Online first for HTML
+            if (event.request.headers.get('Accept').indexOf('text/html') !== -1) {
+                fetch(event.request).then(response => {
+                    if (response.ok)
+                        addToCache(pagesCacheName, request, response.clone());
+                    return response;
+                })
+            }
+            else if (response) {
+                console.log('Retrieving from cache...');
+                return response;
+            }
+            console.log('Retrieving from URL...');
+            return fetch(event.request).catch(function (event) {
+                console.log('Fetch request failed!');
+            });
+        })
+    );
 });
